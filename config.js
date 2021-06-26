c064d9a2ed796d5978831b7cd6363824eb8491fb
Pablo Isaac <pabloisaac@MacBook-Pro-de-Pablo.local>
2021-06-26 01:10:45 -0300

    set port https

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8443
 const host = process.env.HOST || 'localhost'
 const domain = `https://${host}:${port}`
 
@@ -10,7 +10,7 @@ const config = {
   APP: {
     DB_URL: `mongodb://localhost:27017/${process.env.DB_NAME}`,
     DB: process.env.DB_NAME,
-    PORT: 443
+    PORT: 8443
   }
 }
 
