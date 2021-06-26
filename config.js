e751e81dcbb37db32a6de493a315adaaa0a11659
Pablo Isaac <pabloisaac@MacBook-Pro-de-Pablo.local>
2021-06-26 14:35:14 -0300

    Test SLL cert

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const port = process.env.PORT || 3443
+const port = 3443
 const host = process.env.HOST || 'localhost'
 const domain = `https://${host}:${port}`
 
@@ -10,7 +10,7 @@ const config = {
   APP: {
     DB_URL: `mongodb://localhost:27017/${process.env.DB_NAME}`,
     DB: process.env.DB_NAME,
-    PORT: process.env.PORT
+    PORT: port
   }
 }
 
