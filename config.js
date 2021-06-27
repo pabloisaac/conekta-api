a98f496af436273914f633a5fbd8c339298f5cdc
Pablo Isaac <pabloisaac@MacBook-Pro-de-Pablo.local>
2021-06-27 20:45:57 -0300

    Updated configs

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const port = w
+const port = 8080
 const host = process.env.HOST || 'localhost'
 const domain = `https://${host}:${port}`
 
 const config = {
   APP: {
-    DB_URL: "mongodb+srv://jupadevmongo:<password>@cluster0.14oa8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    DB_URL: `mongodb://localhost:27017/${process.env.DB_NAME}` || process.env.DB_URL,
     DB: process.env.DB_NAME,
     PORT: port
   }
